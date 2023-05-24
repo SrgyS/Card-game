ba36b46dfe6d049ce8317581dcf59098f4f7f997
SrgyS <stepanov_sr@mail.ru>
2023-05-24 13:32:45 +0300

    difficulty select in a <form> tag, add radio input type

--- js/components/difficulty-page-component.js
+++ js/components/difficulty-page-component.js
@@ -3,50 +3,102 @@ import { CARDS_PAGE } from "../routes.js";
 
 export function renderDifficultyPageComponent ({appEl, goToPage}) {
 
-    const appHtml = `<div class="difficulty-box">
+    const appHtml = `<form class="difficulty-form">
+                    <div class="difficulty-box">
                         <p class="difficulty-box__title">Выбери сложность</p>
                         <div class="difficulty-box__container">
-                            <div class="difficulty-box__item">1</div>
-                            <div class="difficulty-box__item">2</div>
-                            <div class="difficulty-box__item">3</div>
+                            <label class="difficulty-box__item">
+                                <input type="radio" name="difficulty" value="1" />
+                                <span class="difficulty-box__item-text">1</span>
+                            </label>
+                            <label class="difficulty-box__item">
+                                <input type="radio" name="difficulty" value="2" />
+                                <span class="difficulty-box__item-text">2</span>
+                            </label>
+                            <label class="difficulty-box__item">
+                                <input type="radio" name="difficulty" value="3" />
+                                <span class="difficulty-box__item-text">3</span>
+                            </label>
                         </div>
-                        <button class="btn" id="start-btn">Старт</button>
+                        <button type="submit" class="btn" id="start-btn">Старт</button>
                         <div class="form-error"></div>
-                    </div>`;
-                    appEl.innerHTML = appHtml;
+                    </div>
+                </form>`;
 
-                    const setError = (message) => {
-                        appEl.querySelector(".form-error").textContent = message;
-                      };
+appEl.innerHTML = appHtml;
 
-              const difficultyBtnElements = document.querySelectorAll(".difficulty-box__item"); 
-              let selectedDifficultyBtn = null;    
+            //         const setError = (message) => {
+            //             appEl.querySelector(".form-error").textContent = message;
+            //           };
 
-              difficultyBtnElements.forEach((difficultyBtnEl, index) => {
-                difficultyBtnEl.addEventListener('click', (e) => {
-                    setError("")
-                    if (selectedDifficultyBtn != null) {
-                        selectedDifficultyBtn.classList.remove("selected")
-                    }
-                    difficultyBtnEl.classList.add("selected")
-                    selectedDifficultyBtn = difficultyBtnEl;
+            //   const difficultyBtnElements = document.querySelectorAll(".difficulty-box__item"); 
+            //   let selectedDifficultyBtn = null;    
 
-                    game.difficulty = index + 1;
-                    game.gameStatus = CARDS_PAGE;
-                    game.cards = game.difficulty * 6
+            //   difficultyBtnElements.forEach((difficultyBtnEl, index) => {
+            //     difficultyBtnEl.addEventListener('click', (e) => {
+            //         setError("")
+            //         if (selectedDifficultyBtn != null) {
+            //             selectedDifficultyBtn.classList.remove("selected")
+            //         }
+            //         difficultyBtnEl.classList.add("selected")
+            //         selectedDifficultyBtn = difficultyBtnEl;
+
+            //         game.difficulty = index + 1;
+            //         game.gameStatus = CARDS_PAGE;
+            //         game.cards = game.difficulty * 6
                     
-                    console.log(game)
-                })
-              })
+            //         console.log(game)
+            //     })
+            //   })
 
-              const startBtnEl = document.getElementById("start-btn")
+            //   const startBtnEl = document.getElementById("start-btn")
 
-              startBtnEl.addEventListener("click", (e) => {
-                if (game.difficulty == null){
-                    setError('Выберите уровень сложности!')
-                } else {
-                    goToPage(CARDS_PAGE)
-                }
-              })
+            //   startBtnEl.addEventListener("click", (e) => {
+            //     if (game.difficulty == null){
+            //         setError('Выберите уровень сложности!')
+            //     } else {
+            //         goToPage(CARDS_PAGE)
+            //     }
+            //   })
+
+
+  const difficultyFormEl = appEl.querySelector(".difficulty-form");
+  const formErrorEl = appEl.querySelector(".form-error");
+
+  const difficultyBtnElements = document.querySelectorAll(".difficulty-box__item"); 
+    let prevSelectedBtn = null;    
+
+    difficultyBtnElements.forEach((difficultyBtnEl, index) => {
+      difficultyBtnEl.addEventListener('click', (e) => {
+          setError("")
+          if (prevSelectedBtn != null) {
+            prevSelectedBtn.classList.remove("selected")
+          }
+          difficultyBtnEl.classList.add("selected")
+          prevSelectedBtn = difficultyBtnEl;
+        })
+    })
+
+  difficultyFormEl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const selectedDifficultyBtn = difficultyFormEl.querySelector(
+      "input[name='difficulty']:checked"
+    );
+
+    if (!selectedDifficultyBtn) {
+      setError("Выберите уровень сложности!");
+      return;
+    }
+
+    const selectedDifficulty = parseInt(selectedDifficultyBtn.value);
+    game.difficulty = selectedDifficulty;
+    game.gameStatus = CARDS_PAGE;
+    game.cards = selectedDifficulty * 6;
+    goToPage(CARDS_PAGE);
+  });
+
+  function setError(message) {
+    formErrorEl.textContent = message;
+  }
 }
 
